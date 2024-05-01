40a162100f6e41909402e84a0fc79649810128c0
james fryer <jamesfryer1998@outlook.com>
2024-05-01 11:07:12 +0100

    Slight changes

--- backtest.py
+++ backtest.py
@@ -1,23 +1,20 @@
-import sys
-sys.path.append('/Users/james/Projects/algo_trading/')
 from datetime import datetime, timedelta
-
 from backtest.initialise import Backtest
 from strategies.test_strategy import *
-import backtrader.strategies as btstrats
+import sys
+
+sys.path.append('/Users/james/Projects/algo_trading/')
 
 list_strats = [TestStrategy, 
                MAcrossover, 
-               RSI2Strategy,
-               SimpleMovingAverageStrategy,
-               BreakoutStrategy,
-               BreakdownStrategy,
+               RSI2Strategy, 
+               SimpleMovingAverageStrategy, 
+               BreakoutStrategy, 
+               BreakdownStrategy, 
                RSIOverboughtOversoldStrategy]
 
-tickers = ["NVDA", "AAPL"]
-
-currencies = ["USD", "JPY", "LEV"]
-
+tickers = ["NVDA", "AAPL", "AMZN", "GOOGL"]
+currencies = ["USD", "JPY", "LEV", "AUD", "CHF", "CAD", "GBP", "HKD", "NZD", "KRW"]
 time = (datetime.today() - timedelta(days=5)).date()
 
 def generate_combinations(strats, tickers, currencies, time):
@@ -25,41 +22,29 @@ def generate_combinations(strats, tickers, currencies, time):
 
     for ticker in tickers:
         for strat in strats:
-            print(strat, ticker)
-            backtest = Backtest(strat, ticker, to_date=time, interval='1m')
-            combinations.append(backtest)
+            combinations.append(Backtest(strat, ticker, to_date=time, interval='1m'))
 
     for currency_1 in currencies:
         for currency_2 in currencies:
-            if currency_1 != currency_2:
-                for strat in strats:
-                    print(strat, currency_1, currency_2)
-                    backtest = Backtest(strat, tsym=currency_1, fsym=currency_2, to_date=time, interval='1m')
-                    combinations.append(backtest)
+            for strat in strats:
+                if currency_1 != currency_2:
+                        combinations.append(Backtest(strat, tsym=currency_1, fsym=currency_2, to_date=time, interval='1m'))
 
     return combinations
 
+def run_backtests(combinations):
+    for test in combinations:
+        print(test.ticker)
+        try:
+            test.run()
+        except ZeroDivisionError as e:
+            print(f"Skipping {test.ticker}: {e}")
+            continue
+
+def print_pnl(combinations):
+    for test in combinations:
+        print(test.pnl)
+
 combinations = generate_combinations(list_strats, tickers, currencies, time)
-
-for i in combinations:
-    print(i.ticker)
-    try:
-        i.run()
-    except ZeroDivisionError as e:
-        print(f"Skipping {i.ticker}: {e}")
-        continue
-
-for i in combinations:
-    print(i.pnl)
-
-
-# Testing
-# Backtest(MAcrossover, 'USD', 'GBP', to_date='2016-01-01').run()
-# Backtest(btstrats.SMA_CrossOver, 'USD', 'GBP', to_date='2016-01-01').run()
-# Backtest(RSI2Strategy, 'NVDA', to_date='2024-03-05', interval='15m').run()
-# Backtest(BreakoutStrategy, 'USD', 'GBP', to_date='2024-03-28', interval='1m').run()
-
-# Backtest(BreakdownStrategy, 'USD', 'GBP', to_date='2023-03-28').run()
-# Backtest(RSI2Strategy, 'USD', 'GBP', to_date='2024-03-28', interval='30m').run()
-# Backtest(RSIOverboughtOversoldStrategy, 'USD', 'GBP', to_date='2024-04-21', interval='1m').run()
-# Backtest(RSIOverboughtOversoldStrategy, 'NVDA', to_date=test, interval='1m').run()
+run_backtests(combinations)
+print_pnl(combinations)
